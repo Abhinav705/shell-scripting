fec4d990216e5b845d00dd428f62986127c80393
Sai Abhinav Chowdary Karnati <saiabhinav_karnati@srmap.edu.in>
2024-07-07 14:56:35 +0530

    ic

---
+++ 05-specialchars.sh
@@ -0,0 +1,13 @@
+var1=$1
+var2=$2
+var3=$3
+
+echo "All variables print $@"
+echo "Number of variables passed $#"
+echo "Script Name $0"
+echo "Current Working Directory $PWD"
+echo "Home Directory of Current User $HOME"
+echo "Which user is running the script $USER"
+echo "HOST NAME : $HOSTNAME"
+echo "Process ID of current shell script: $$"
+echo "Process ID of last background command $!"
