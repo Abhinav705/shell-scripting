cc313bf41fc10cb8bd102c4a0639636ee6f4af56
Sai Abhinav Chowdary Karnati <saiabhinav_karnati@srmap.edu.in>
2024-07-07 22:03:56 +0530

    ic

--- 07-functions.sh
+++ 07-functions.sh
@@ -4,14 +4,18 @@ userid=$(id -u)
 DATETIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$DATETIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "installation of $2 is failed"
+        echo "installation of $2 is $R failed $N"
         exit 1
     else
-        echo "installation of $2 is success"
+        echo "installation of $2 is $G success $N"
     fi
 
 }
