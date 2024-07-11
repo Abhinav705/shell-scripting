35453d546143309b3d9386eaa2c06ed8f5adeda9
Sai Abhinav Chowdary Karnati <saiabhinav_karnati@srmap.edu.in>
2024-07-11 23:07:52 +0530

    IC

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -62,7 +62,7 @@ npm install &>>$LOG_FILE
 VALIDATE $? "Installing dependencies"
 
 
-cp backend.service /etc/systemd/system/backend.service
+cp /home/ec2-user/shell-scripting/expense-shell/backend.service  /etc/systemd/system/backend.service &>>$LOG_FILE
 VALIDATE $? "Copied backend service"
 
 systemctl daemon-reload &>>$LOG_FILE
