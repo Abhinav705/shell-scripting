2b6321aadf623efa98a7294b9c658b36998420b0
Sai Abhinav Chowdary Karnati <saiabhinav_karnati@srmap.edu.in>
2024-07-14 18:30:29 +0530

    IC

---
+++ expense-shell/10-disk-alert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DISK_MEM=$(df -hT | grep xfs)
+LIMIT=5
+Message=""
+
+while IFS= read -r line
+do
+    PERCENTAGE=$(echo $line | awk -F " " '{print $6F}'| cut -d "%" -f1)
+    FOLDER=$(echo $line| awk -F " " '{print $NF}')
+    if [ $PERCENTAGE -ge $LIMIT ]
+    then
+        Message += "Disk Usage reached the limit: Folder is : $FOLDER and current usage is: $PERCENTAGE"
+    fi
+done <<<$DISK_MEM
+echo "$Message"
